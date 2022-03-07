1547b68c83d6f25415e123b181c1c910c283fd08
Hadi Channaa <aahdi600@gmail.com>
2022-03-07 10:53:31 -0600

    completed calculator

--- src/main/java/com/hcl/App.java
+++ src/main/java/com/hcl/App.java
@@ -23,28 +23,84 @@ import java.util.Scanner;
 public class App {
 	public static void main(String[] args) {
 		//Poor mans banner
-		System.out.println("Hello World! I am a great calculator :-) ;-) ");
+		System.out.println("The Great 4 Operation Calculator ^.^");
 		
 		// creates an object of Scanner
 	    Scanner input = new Scanner(System.in);
+	    boolean go = true;
+	    while(go) {
+	    	System.out.println("Please enter an operation from this list. +, -, /, *: ");
+		    String operation = input.next();
+			
+		    System.out.print("Enter first number: ");
+		    // takes 1st number
+		    int firstNumber = input.nextInt();
 
-	    System.out.print("Enter first number: ");
+		    System.out.println("Enter second number: ");
+		    // takes 2st number
+		    int secondNumber = input.nextInt();
 
-	    // takes 1st number
-	    int firstNumber = input.nextInt();
-
-	    // prints the name
-	    System.out.println("Enter second number: ");
-	    
-	    // takes 2st number
-	    int secondNumber = input.nextInt();
-	    
-	    //operation add only
-	    int sum = firstNumber + secondNumber;
-	    
-	    System.out.printf("Sum of %d and %d = %d", firstNumber, secondNumber, sum);
+		    
+		    if(operation.equals("+")) {
+		    	add(firstNumber, secondNumber);
+		    }else if(operation.equals("-")) {
+		    	subtract(firstNumber, secondNumber);
+		    }else if(operation.equals("/")) {
+		    	divide(firstNumber, secondNumber);
+		    }else if(operation.equals("*")) {
+		    	multiply(firstNumber, secondNumber);
+		    }else {
+		    	System.out.println("Invalid operation entered.");
+		    }
+		    
+		    System.out.println("Would you like to do another operation? input Y or N: ");
+		    boolean gogo = true;
+		    while(gogo) {
+		    	String repeat = input.next();
+			    if(repeat.toUpperCase().equals("Y")) {
+			    	gogo = false;
+			    }else if(repeat.toUpperCase().equals("N")) {
+			    	go = false;
+			    	gogo = false;
+			    }else {
+			    	System.out.println("invalid input, try again. input Y or N: ");	    	
+			    }
+		    }
+		    
+	    }
 
 	    // closes the scanner
 	    input.close();
 	}
+	
+	public static void add(int firstNumber, int secondNumber) {
+		//operation add only
+	    int sum = firstNumber + secondNumber;
+	    
+	    System.out.printf("Sum of %d and %d = %d %n", firstNumber, secondNumber, sum);
+	}
+	
+	public static void subtract(int firstNumber, int secondNumber) {
+		//operation add only
+	    int difference = firstNumber - secondNumber;
+	    
+	    System.out.printf("Difference of %d and %d = %d %n", firstNumber, secondNumber, difference);
+	}
+	
+	public static void divide(int firstNumber, int secondNumber) {
+		//operation add only
+	    int quotient = firstNumber / secondNumber;
+	    
+	    System.out.printf("Quotient of %d and %d = %d %n", firstNumber, secondNumber, quotient);
+	}
+	
+	public static void multiply(int firstNumber, int secondNumber) {
+		//operation add only
+	    int product = firstNumber * secondNumber;
+	    
+	    System.out.printf("Product of %d and %d = %d %n", firstNumber, secondNumber, product);
+	}
+	
+	
+	
 }
